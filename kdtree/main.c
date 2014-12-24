c62c447bff8b2084914eb16352b30367cc72eb22
Luo Qian <wqian94@mit.edu>
2014-12-23 19:41:43 -0500

    Rewrote main.c to align with changes to kdtree

--- kdtree/main.c
+++ kdtree/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "./Quadtree.h"
+#include "./KDtree.h"
 
 int main(int argc, char* argv[]) {
     setbuf(stdout, 0);
-    //Quadtree qt = Quadtree_create(1024, Point_create(0, 0));
-    //printf("%f %f %f\n", qt.center.x, qt.center.y, qt.length);
+    KDtree* qt = KDtree_create(1024, (Point){0, 0});
+    printf("%lf %lf %f\n", qt->center.val[0], qt->center.val[1], qt->length);
     return 0;
 }
